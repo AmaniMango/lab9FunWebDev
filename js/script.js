f334dcb30703ef3d1c890315335ddce8f72582a6
amanicps <80164449+AmaniMango@users.noreply.github.com>
2024-11-05 22:37:22 -0600

    fixed formatting

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 function init(){
-    var button = document.getElementById('entrybutton');
-  
-    function showMeText(){
-      var textbox = document.getElementById('entryinput');
-      document.getElementById('textoutput').innerHTML = textbox.value;
-      alert("Amani Mohammad: " + textbox.value);
-    }
-  
-    button.addEventListener('click', showMeText);
-    }
-    
-  window.addEventListener('load', init);
+  var button = document.getElementById('entrybutton');
+  function showMeText(){
+    var textbox = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = textbox.value;
+    alert("Amani Mohammad: " + textbox.value);
+  }
+  button.addEventListener('click', showMeText);
+}
+
+window.addEventListener('load', init);
